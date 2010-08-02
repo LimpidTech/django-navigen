fcf55b047ced4825f961c3d40f6f8af00ce46b3a
Brandon R. Stoner <monokrome@monokro.me>
2010-08-01 23:00:59 -0600

    Added a setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils import setup
+
+setup(name='django navigation',
+      version='0.1',
+      description='An application for generating navigation menus dynamically in django',
+      author='Brandon R. Stoner',
+      author_email='monokrome@monokro.me',
+      packages=['django.contrib.navigation'],
+)
+
