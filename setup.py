526989bccadbc9cb710e3dd88e3fa8e0592df509
Brandon R. Stoner <github@monokro.me>
2010-11-02 18:16:35 -0700

    Modified setup.py to use setuptools, and now I can include package data.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='django navigation',
       version='0.1',
       description='An application for generating navigation menus dynamically in django',
       author='Brandon R. Stoner',
       author_email='monokrome@monokro.me',
-      packages=[
-            'navigation',
-            'navigation.templatetags',
-      ],
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      classifiers=[
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'Operating System :: OS Independent',
+    ],
 )
 
